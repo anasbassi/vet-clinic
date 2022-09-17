b40a7733b37b90b1cf958cddb8178cd000d422a8
Anas Abubakar <anasabubakar2015@gmail.com>
2022-09-17 08:42:20 +0100

    Add vet, specialization and visits table

--- queries.sql
+++ queries.sql
@@ -42,3 +42,60 @@ SELECT species.name, COUNT(*) FROM animals JOIN species ON animals.species_id = 
 SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id JOIN species ON animals.species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
 SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0 GROUP BY animals.name;
 SELECT owners.full_name, COUNT(*) FROM owners JOIN animals ON owners.id = animals.owners_id GROUP BY owners.full_name ORDER BY COUNT(*) DESC LIMIT 1;
+
+-- Who was the last animal seen by William Tatcher?
+SELECT animals.name FROM animals
+JOIN visits ON visits.animals_id = animals.id
+WHERE vets_id = 1
+ORDER BY date_of_visit DESC
+LIMIT 1;
+
+-- How many different animals did Stephanie Mendez see?
+SELECT animals.name FROM animals
+JOIN visits ON visits.animals_id = animals.id
+WHERE vets_id = 3;
+
+-- List all vets and their specialties, including vets with no specialties.
+SELECT vets.name, species.name FROM vets
+LEFT JOIN specializations ON specializations.vets_id = vets.id
+LEFT JOIN species ON specializations.species_id = species.id;
+
+-- List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+SELECT animals.name, visits.date_of_visit FROM animals
+JOIN visits ON visits.animals_id = animals.id
+WHERE vets_id = 3 AND visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30';
+
+-- What animal has the most visits to vets?
+SELECT COUNT(visits.animals_id) as visit_count, animals.name from animals
+JOIN visits On animals.id = visits.animals_id
+GROUP BY visits.animals_id, animals.name
+ORDER BY visit_count DESC
+LIMIT 1;
+
+-- Who was Maisy Smith's first visit?
+SELECT animals.name, vets.name, visits.date_of_visit FROM animals
+JOIN visits ON animals.id = visits.animals_id
+JOIN vets ON visits.vets_id = vets.id
+WHERE visits.vets_id = 2
+ORDER BY date_of_visit ASC
+LIMIT 1;
+
+-- Details for most recent visit: animal information, vet information, and date of visit.
+SELECT animals.name, vets.name, visits.date_of_visit FROM animals
+JOIN visits ON animals.id = visits.animals_id
+JOIN vets ON visits.vets_id = vets.id
+WHERE visits.date_of_visit = '2021-05-04';
+
+-- How many visits were with a vet that did not specialize in that animal's species?
+SELECT COUNT(*) FROM animals
+INNER JOIN visits ON animals.id = visits.animals_id
+INNER JOIN vets ON vets.id = visits.vets_id WHERE vets.id = 2;
+
+-- What specialty should Maisy Smith consider getting? Look for the species she gets the most.
+SELECT COUNT(visits.animals_id) AS count_visits, species.name FROM visits
+JOIN animals ON animals.id = visits.animals_id
+JOIN species ON animals.species_id = species.id
+WHERE visits.vets_id = 2
+GROUP BY species.name
+ORDER BY count_visits DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -27,3 +27,29 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  age INT,
+  date_of_graduation DATE,  
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+  species_id INT,
+  vets_id INT,
+  FOREIGN kEY (species_id) REFERENCES species(id),
+  FOREIGN kEY (vets_id) REFERENCES vets(id),
+  PRIMARY kEY (species_id, vets_id)
+);
+
+CREATE TABLE visits (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animals_id INT,
+  vets_id INT,
+  date_of_visit DATE,
+  FOREIGN KEY (animals_id) REFERENCES animals(id),
+  FOREIGN KEY (vets_id) REFERENCES vets(id),
+  PRIMARY KEY (id)
+);
